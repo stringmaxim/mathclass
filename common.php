fd1ba2db40f588d08db19eed20aedeca0d3274e5
Maxim <slufy123@gmail.com>
2015-04-02 11:47:07 -0700

    1

--- common.php
+++ common.php
@@ -1,26 +1,20 @@
 <?php 
-
-    // These variables define the connection information for your MySQL database 
     $username = "root"; 
     $password = ""; 
     $host = "localhost"; 
     $dbname = "bobbie"; 
 
-
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
-     
 
     try 
     { 
-
         $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); 
     } 
     catch(PDOException $ex) 
     { 
-
         die("Failed to connect to the database: " . $ex->getMessage()); 
     } 
-     
+
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
      
@@ -45,7 +39,7 @@
         undo_magic_quotes_gpc($_GET); 
         undo_magic_quotes_gpc($_COOKIE); 
     } 
-
-    header('Content-Type: text/html; charset=utf-8'); 
      
+    header('Content-Type: text/html; charset=utf-8'); 
+
     session_start(); 
